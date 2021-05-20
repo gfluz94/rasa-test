f7b9d802a9bf088585c3ddb41a7ae9776660c207
Gabriel Luz <gabriel.luz@picpay.com>
2021-05-20 12:24:12 -0300

    correct results path

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-with open("./results/pull_request_report.md", "w") as file:
+with open("results/pull_request_report.md", "w") as file:
     file.write("## HEY!!!!\n\nAqui é o Gabriel pedindo o *PULL REQUEST* :P")
